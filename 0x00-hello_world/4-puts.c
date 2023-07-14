8aad301a82ed57aba762ce8776d066982aabd2d9
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-07-13 21:23:25 -0700

    Puts4Printf

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+/* 
+ * File: 4-puts.c 
+ * Auth: Oluwashina "Sina" Stephen Mathew <mathewoluwashinastephen@gmail.com> 
+ */ 
+  
+ #include <stdio.h> 
+  
+ /*
+  * main - Prints "Programming is like building a multilingual puzzle, followed by a new line. 
+  * Return: Always 0. 
+  */
+ int main(void) 
+	 { 
+		          puts("\"Programming is like building a multilingual puzzle"); 
+			    
+			           return (0); 
+				    }
