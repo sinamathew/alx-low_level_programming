e4732617026008122f7e574256361f7273d9e9ab
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-08-14 16:15:17 -0700

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,12 +9,13 @@
  * By: Sina Mathew
  */
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif /* DOG_H */
