bedd87bfea82ad59181338ca1ef7bc4f0390a2aa
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-08-14 16:18:16 -0700

    dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -7,11 +7,13 @@
  * @name: C isn't fun
  * @age: C isn't fun
  * @owner: C isn't fun
+ *
+ * Return: void
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d != NULL)
+	if (d)
 	{
 		d->name = name;
 		d->age = age;
