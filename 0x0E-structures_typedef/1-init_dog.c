0af1a7396189b4f849e6856c504fd089264605bd
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-08-14 16:04:16 -0700

    dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -3,6 +3,10 @@
 /**
  * init_dog - function that initialize a variable of type struct dog
  * By: Sina Mathew
+ * @d: C isn't fun
+ * @name: C isn't fun
+ * @age: C isn't fun
+ * @owner: C isn't fun
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
