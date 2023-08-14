a787be4d47312adcf8b041c65b749c8b31697e1f
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-08-14 16:44:27 -0700

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,10 +3,11 @@
 
 /**
  * struct dog - C isn't fun
- * struct dog_t - C isn't fun
  * @name: C isn't fun
  * @age: C isn't fun
  * @owner: C isn't fun
+ * dog_t - C isn't fun
+ *
  * By: Sina Mathew
  */
 
@@ -17,12 +18,7 @@ struct dog
 	char *owner;
 };
 
-struct dog_t
-{
-	char *name;
-	float age;
-	char *owner;
-};
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
