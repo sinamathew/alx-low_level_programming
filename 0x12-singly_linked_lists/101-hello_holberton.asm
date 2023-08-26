863d507734a4a741f31303079a8e19896f4590f3
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-08-25 19:14:24 -0700

    Update Task 6 file

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,17 +1,18 @@
 section .data
-    hello_fmt db "Hello, Holberton", 10, 0  ; Format string for printf
+	hello_fmt db "Hello, Holberton", 10, 0  ; Format string for printf
 
 section .text
-    global main
+	global main
+	extern printf
 
 main:
-    ; Prepare arguments for printf
-    mov rdi, hello_fmt  ; Format string address
-    xor rax, rax        ; Clear RAX (no floating-point arguments)
-    call printf
+	; Prepare arguments for printf
+	mov rdi, hello_fmt  ; Format string address
+	xor rax, rax        ; Clear RAX (no floating-point arguments)
+	call printf
 
-    ; Exit the program
-    mov rax, 60         ; syscall number for exit
-    xor rdi, rdi        ; status code 0
-    syscall
+	; Exit the program
+	mov rax, 60         ; syscall number for exit
+	xor rdi, rdi        ; status code 0
+	syscall
 
