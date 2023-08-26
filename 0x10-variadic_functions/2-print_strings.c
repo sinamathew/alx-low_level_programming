8eaf25a6883f01c8b6a9243c187158734fa1265f
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-08-26 06:48:03 -0700

    Task 2 Update and fix

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -20,7 +20,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		str = va_arg(args,char *);
+		str = va_arg(args, char *);
 		if (str == NULL)
 			printf("(nil)");
 		else
