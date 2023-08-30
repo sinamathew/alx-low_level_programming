6bd7e1b6021474d3c7bd42b20bf850090802120b
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-08-29 19:41:37 -0700

    Added task 1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * listint_len - returns the number of elements in a linked listint_t list
+ * @h: points to the head of the element.
+ * By: Sina Mathew
+ *
+ * Return: count, number of element
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count)
+}
