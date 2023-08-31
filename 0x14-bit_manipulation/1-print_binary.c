2a40e20b811f82a26e80c3c72e4f8c8c1af829ca
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-08-31 14:20:54 -0700

    Added task 1 file

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,29 @@
+#innclude "main.h"
+
+/**
+ * print_binary - prints the binary representation of a number.
+ * @n: takes the number parameter
+ * By: Sina Mathew
+ */
+
+void print_binary(unsigned long int n)
+{
+	unsigned long int mask;
+
+	mask = 1 << (sizeof(n) * 8 -1);
+	if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+
+	while (mask > 0)
+	{
+		if ((n & mask) == 0)
+			_putchar('0');
+		else
+			_putchar('0');
+
+		mask >>= 1;
+	}
+}
