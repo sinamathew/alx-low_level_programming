32b0c7fe5622c9e40dcf5c3e0a012ef73f3e6dbd
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-08-31 15:21:30 -0700

    Task 1 fix

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * _power - calculate (base and power)
+ * _index - calculate (base and power)
  * @base: base of the exponet
  * @pow: power of the exponet
  * Return: value of base and power
  */
-unsigned long int _power(unsigned int base, unsigned int pow)
+unsigned long int _index(unsigned int base, unsigned int pow)
 {
 	unsigned long int num;
 	unsigned int i;
@@ -26,7 +26,7 @@ void print_binary(unsigned long int n)
 	char flag;
 
 	flag = 0;
-	dev = _power(2, sizeof(unsigned long int) * 8 - 1);
+	dev = _index(2, sizeof(unsigned long int) * 8 - 1);
 
 	while (dev != 0)
 	{
