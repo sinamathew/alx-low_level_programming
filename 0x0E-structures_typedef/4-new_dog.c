9514267a609150bbecaeb311b31e9a6fb889979d
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-09-02 02:17:43 -0700

    fix task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,7 +14,12 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_dog = malloc(sizeof(dog_t));
+	dog_t *new_dog;
+
+	if (name == '\0' || owner == '\0')
+		return (NULL);
+
+	new_dog = malloc(sizeof(dog_t));
 
 	if (new_dog == NULL)
 		return (NULL);
