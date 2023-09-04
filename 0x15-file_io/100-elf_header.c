6a0b12da94a030f4174c960f21e9bb463e9eb658
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-09-04 16:26:06 -0700

    Update task 100 & header

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -40,11 +40,17 @@ int open_elf_file(const char *filename)
  */
 void print_elf_header_info(Elf64_Ehdr header)
 {
+	int i;
+
 	printf("ELF Header:\n");
-	printf("  Magic:   %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x\n",
-			header.e_ident[EI_MAG0], header.e_ident[EI_MAG1], header.e_ident[EI_MAG2], header.e_ident[EI_MAG3],
-			header.e_ident[EI_CLASS], header.e_ident[EI_DATA], header.e_ident[EI_VERSION], header.e_ident[EI_OSABI],
-			header.e_ident[EI_ABIVERSION], header.e_ident[EI_PAD], header.e_ident[EI_NIDENT]);
+	printf("  Magic:   ");
+	for (i = 0; i < EI_NIDENT; i++)
+	{
+		printf("%02x", header.e_ident[i]);
+		if (i < EI_NIDENT - 1)
+			printf(" ");
+	}
+	printf("\n");
 	printf("  Class:                             ELF64\n");
 	printf("  Data:                              2's complement, little endian\n");
 	printf("  Version:                           1 (current)\n");
@@ -56,14 +62,16 @@ void print_elf_header_info(Elf64_Ehdr header)
 
 int main(int argc, char *argv[])
 {
+	Elf64_Ehdr header;
+	int fd;
+
 	if (argc != 2)
 	{
 		dprintf(2, "Usage: %s elf_filename\n", argv[0]);
 		exit(97);
 	}
 
-	int fd = open_elf_file(argv[1]);
-	Elf64_Ehdr header;
+	fd = open_elf_file(argv[1]);
 
 	if (lseek(fd, 0, SEEK_SET) == -1 || read(fd, &header, sizeof(header)) != sizeof(header))
 	{
