9d4cd923bfb9d37f564bac847686529d66f314e6
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-09-04 16:11:07 -0700

    Added task 100 & update header

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,10 +6,13 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <string.h>
+#include <elf.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int copy_file(const char * from_filename, const char *to_filename);
+int open_elf_file(const char *filename);
+void print_elf_header_info(Elf64_Ehdr header);
 
 #endif /* MAIN_H */
