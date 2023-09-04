32bdb37801a0275e3b196ca2ca6abcf8a7a885cf
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-09-04 15:21:12 -0700

    Fix task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -17,7 +17,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 
 	fdo = open(filename, O_WRONLY | O_APPEND);
-	if (fd == -1)
+	if (fdo == -1)
 		return (-1);
 
 	if (text_content != NULL)
