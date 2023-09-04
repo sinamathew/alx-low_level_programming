b1d10f4226c350b6b9f65adf1b294ff836da4927
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-09-04 14:57:08 -0700

    Update header and fix task 1

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,32 +2,50 @@
 
 /**
  * read_textfile - reads and prints a text file to the POSIX standard output
- * filename: name of the file
- * letters: the number of letters it should read and print
+ * @filename: name of the file
+ * @letters: the number of letters it should read and print
  * By: Sina Mathew
  *
  * Return: if the file can not be opened or read, return 0
- * if filename is NULL return 0
- * if write fails or does not write the expected amount of bytes, return 0
+ *	if filename is NULL return 0
+ *	if write fails or does not write the expected amount of bytes, return 0
+ *	otherwise, return the actual number of letters read and printed
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
 	ssize_t n;
-	char buf[letters];
+	char *buf;
+
+	if (filename == NULL)
+		return (0);
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 
-	n = read(fd, buf, letters);
-	close(fd);
-
-
-	if (n < 0)
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
+	{
+		close(fd);
 		return (0);
+	}
 
-	write(STDOUT_FILENO, buf, n);
+	n = read(fd, buf, letters);
+	if (n == -1)
+	{
+		close(fd);
+		free(buf);
+		return (0);
+	}
+
+	n = write(STDOUT_FILENO, buf, n);
+
+	close(fd);
+	free(buf);
+
+	if (n == -1)
+		return (0);
 
 	return (n);
 }
