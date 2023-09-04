384978a79d7f3320ee4da242d42b3b290f5f5283
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-09-04 15:52:15 -0700

    Fix task 3 & update header

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,7 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
+#include "main.h"
 
 /**
  * main - copies the content of one file to another
@@ -13,26 +10,41 @@
  */
 int main(int argc, char *argv[])
 {
-	int fd_from, fd_to, rd, wr;
-	char buf[1024];
-
 	if (argc != 3)
 	{
 		dprintf(2, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 
-	fd_from = open(argv[1], O_RDONLY);
+	copy_file(argv[1], argv[2]);
+
+	return (0);
+}
+
+/**
+ * copy_file - copies the content of one file to another
+ * @from_filename: name of the source file
+ * @to_filename: name of the destination file
+ * By: Sina Mathew
+ *
+ * Return: 0 on success, of exit with error codes on failure
+ */
+int copy_file(const char * from_filename, const char *to_filename)
+{
+	int fd_from, fd_to, rd, wr;
+	char buf[1024];
+
+	fd_from = open(from_filename, O_RDONLY);
 	if (fd_from == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(2, "Error: Can't read from file %s\n", from_filename);
 		exit(98);
 	}
 
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	fd_to = open(to_filename, O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
-		dprintf(2, "Error: Can't write to %s\n", argv[2]);
+		dprintf(2, "Error: Can't write to %s\n", to_filename);
 		close(fd_from);
 		exit(99);
 	}
@@ -42,7 +54,7 @@ int main(int argc, char *argv[])
 		wr = write(fd_to, buf, rd);
 		if (wr != rd)
 		{
-			dprintf(2, "Error: Can't write to %s\n", argv[2]);
+			dprintf(2, "Error: Can't write to %s\n", to_filename);
 			close(fd_from);
 			close(fd_to);
 			exit(99);
@@ -51,7 +63,7 @@ int main(int argc, char *argv[])
 
 	if (rd == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(2, "Error: Can't read from file %s\n", from_filename);
 		close(fd_from);
 		close(fd_to);
 		exit(98);
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -10,5 +10,6 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int copy_file(const char * from_filename, const char *to_filename);
 
 #endif /* MAIN_H */
