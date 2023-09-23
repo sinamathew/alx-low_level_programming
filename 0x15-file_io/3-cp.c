229b8048165cdfe1c4aa0cc0126635c2cd3f9d44
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-09-23 11:05:50 +0100

    Fix betty

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -24,6 +24,45 @@ int main(int argc, char *argv[])
 
 
 /**
+ * open_source_file - Opens the source file for reading
+ * @from_filename: name of the source file
+ * @fd_from: pointer to the file descriptor of the source file
+ * By: Sina Mathew
+ *
+ * Return: ) on success, or exit with error codes on failure
+ */
+int open_source_file(const char *from_filename, int *fd_from)
+{
+	*fd_from = open(from_filename, O_RDONLY);
+	if (*fd_from == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n" from_filename);
+		return (98);
+	}
+	return (0);
+}
+
+
+/**
+ * open_dest_file - Opens the destination file for writing
+ * @to_filename: name of the ddestination file
+ * @fd_to: pointer to the file descriptor of the destination file
+ *
+ * Return: 0 on success, or exit with error codes failure
+ */
+int open_dest_file(const char *to_filename, int *fd_to)
+{
+	*fd_to = open(to_filename, O_WRONLY | O_CREAT \ O_TRUNc, 0664);
+	if (*fd_to == -1)
+	{
+		dprintf(2, "Error: Can't write to %s\n", to_filename);
+		return (99);
+	}
+	return (0);
+}
+
+
+/**
  * copy_file - copies the content of one file to another
  * @from_filename: name of the source file
  * @to_filename: name of the destination file
@@ -36,21 +75,13 @@ int copy_file(const char *from_filename, const char *to_filename)
 	int fd_from, fd_to, rd, wr;
 	char buf[1024];
 
-	fd_from = open(from_filename, O_RDONLY);
-	if (fd_from == -1)
+	if (open_source_file(from_filename, &fd_from) != 0)
+		return (98);
+	if (open_dest_file(to_filename, &fd_to) != 0)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", from_filename);
-		exit(98);
-	}
-
-	fd_to = open(to_filename, O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fd_to == -1)
-	{
-		dprintf(2, "Error: Can't write to %s\n", to_filename);
 		close(fd_from);
-		exit(99);
+		return (99);
 	}
-
 	while ((rd = read(fd_from, buf, 1024)) > 0)
 	{
 		wr = write(fd_to, buf, rd);
@@ -62,7 +93,6 @@ int copy_file(const char *from_filename, const char *to_filename)
 			exit(99);
 		}
 	}
-
 	if (rd == -1)
 	{
 		dprintf(2, "Error: Can't read from file %s\n", from_filename);
@@ -70,19 +100,16 @@ int copy_file(const char *from_filename, const char *to_filename)
 		close(fd_to);
 		exit(98);
 	}
-
 	if (close(fd_from) == -1)
 	{
 		dprintf(2, "Error: Can't close fd %d\n", fd_from);
 		exit(100);
 	}
-
 	if (close(fd_to) == -1)
 	{
 		dprintf(2, "Error: Can't close fd %d\n", fd_to);
 		exit(100);
 	}
-
 	return (0);
 }
 
