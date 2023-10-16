74f30506d84ac1477afbd0a9132696c09fc57fc1
Sina Mathew <mathewoluwashinastephen@google.com>
2023-10-16 19:51:26 +0100

    is lower

---
+++ 0x18-dynamic_libraries/_islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _islower - check if letter is uppercase
+ * @c: the letter
+ *
+ * Return: I don't know yet
+ *
+ * By: Sina Mathew
+ */
+int _islower(int c)
+{
+	return (0);
+}
