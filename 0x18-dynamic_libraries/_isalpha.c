0c0cec82ebd66428cc7c621f50f1ecc68953834a
Sina Mathew <mathewoluwashinastephen@google.com>
2023-10-16 19:55:59 +0100

    Is alpha

---
+++ 0x18-dynamic_libraries/_isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isalpha - check if letter is alpha
+ * @c: the letter
+ *
+ * Return: I don't know yet
+ *
+ * By: Sina Mathew
+ */
+int _isalpha(int c)
+{
+	return (0);
+}
