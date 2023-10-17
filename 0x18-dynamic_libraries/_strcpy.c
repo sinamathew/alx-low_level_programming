5e5c407f1e1d18b08f509397b77b51583b30da46
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-10-17 03:29:11 +0100

    String Copy

---
+++ 0x18-dynamic_libraries/_strcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strcpy - copy string
+ * @dest: where to copy to
+ * @src: where to copy from
+ *
+ * Return: Don't know yet
+ *
+ * By; Sina Mathew
+ */
+char *_strcpy(char *dest, char *src)
+{
+	return (0);
+}
