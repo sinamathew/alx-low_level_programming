92ab01c83f2bc57e3f2f5103cb2dd53672bbf834
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-10-17 03:22:05 +0100

    String lenght

---
+++ 0x18-dynamic_libraries/_strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen - count string lenght
+ * @s: pointer to the string
+ *
+ * Return: number of strings counted
+ *
+ * By: Sina Mathew
+ */
+int _strlen(char *s)
+{
+	return (0)
+}
