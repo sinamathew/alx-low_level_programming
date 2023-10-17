c8486506b6c7e663891fda4e296189e00404d846
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-10-17 03:24:56 +0100

    Puts

---
+++ 0x18-dynamic_libraries/_puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts - put character to standard output
+ * @s: The character
+ *
+ * Description: Similar to _putchar
+ *
+ * By: Sina Mathew
+ */
+void _puts(char *s)
+{
+
+}
