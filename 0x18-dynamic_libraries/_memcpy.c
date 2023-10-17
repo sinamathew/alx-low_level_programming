fd7cec837a26bce865683b7fc88d7962c67e16a9
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-10-17 03:55:01 +0100

    Memory Copy

---
+++ 0x18-dynamic_libraries/_memcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _memcpy - setup memory
+ * @dest: pointer to source
+ * @src: pointer to destination
+ * @n: Don't know yet
+ *
+ * Return: I don't know yet
+ *
+ * By: Sina Mathew
+ */
+char *_memset(char *dest, char *src, unsigned int n)
+{
+	return (0);
+}
