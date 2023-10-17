9a81de596775b9ac222cc4379a67ef34f31009ee
Sina Mathew <mathewoluwashinastephen@google.com>
2023-10-17 12:39:37 +0100

    Fixed betty issue

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -1,25 +1,60 @@
 #include <stdio.h>
 
+/**
+ * add - add two long integers
+ * @a: first int
+ * @b: second int
+ *
+ * Return: a + b
+ */
 long add(long a, long b)
 {
 	return (a + b);
 }
 
+/**
+ * sub - subtract two long integers
+ * @a: first int
+ * @b: second int
+ *
+ * Return: a - b
+ */
 long sub(long a, long b)
 {
 	return (a - b);
 }
 
+/**
+ * mul - multiply two long integers
+ * @a: first int
+ * @b: second int
+ *
+ * Return: a * b
+ */
 long mul(long a, long b)
 {
 	return (a * b);
 }
 
+/**
+ * div - divide two long integers
+ * @a: first int
+ * @b: second int
+ *
+ * Return: a / b
+ */
 long div(long a, long b)
 {
 	return (a / b);
 }
 
+/**
+ * mod - find remainder
+ * @a: first int
+ * @b: second int
+ *
+ * Return: a % b
+ */
 long mod(long a, long b)
 {
 	return (a % b);
