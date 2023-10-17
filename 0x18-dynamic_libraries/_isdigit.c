53e358a29d3fa5f1e2ff2b9f107bd8edc192ee1a
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-10-17 03:19:04 +0100

    Digit

---
+++ 0x18-dynamic_libraries/_isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isdigit - check for digit
+ * @c: the interger passed
+ *
+ * Return: Don't know yet
+ *
+ * By: Sina Mathew
+ */
+int _isdigit(int c)
+{
+	return (0);
+}
