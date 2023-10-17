401737532a4e7bc47745bbb20bdb92be71cdb608
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-10-17 03:42:05 +0100

    String

---
+++ 0x18-dynamic_libraries/_strncat.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strncat - opposite of _strcat
+ * @dest: pointer to destination
+ * @src: pointer to src
+ * @n: I don't know
+ *
+ * Return: I don't know
+ *
+ * By: Sina Mathew
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	return (0);
+}
+
