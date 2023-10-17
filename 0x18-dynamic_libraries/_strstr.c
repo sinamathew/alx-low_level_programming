82765b65cf362b9b046916dda80d698c25e96b24
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-10-17 04:12:36 +0100

    String to String

---
+++ 0x18-dynamic_libraries/_strstr.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strstr - String String :)
+ * @haystack: pointer to stack
+ * @needle: pointer to needle
+ *
+ * Return: Don't know
+ *
+ * By: Sina Mathew
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	return (0);
+}
+
