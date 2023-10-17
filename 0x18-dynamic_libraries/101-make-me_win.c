1c9795b8587d6f18f73a0016f7684796632ea797
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-10-17 04:51:38 +0100

    Win

---
+++ 0x18-dynamic_libraries/101-make-me_win.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int atoi(const char *str)
+{
+	(void) str;
+
+	puts("9 8 10 24 75 - 9");
+	puts("Congratulations, you win the Jackpot!");
+	exit(0);
+	return (1);
+}
