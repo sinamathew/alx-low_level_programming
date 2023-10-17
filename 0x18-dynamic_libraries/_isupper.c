80d0deee92deb032c21a015397a9d921e125b9d6
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-10-17 03:15:58 +0100

    Uppercase

---
+++ 0x18-dynamic_libraries/_isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isupper - check if letter is uppercase
+ * @c: the letter
+ *
+ * Return: I don't know yet
+ *
+ * By: Sina Mathew
+ */
+int _isupper(int c)
+{
+	return (0)
+}
