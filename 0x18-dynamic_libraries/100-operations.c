ee5e786c6d0dedaeed0ca780b7acebcfa9874b4c
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-10-17 04:55:46 +0100

    Everywher good

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+long add(long a, long b)
+{
+	return (a + b);
+}
+
+long sub(long a, long b)
+{
+	return (a - b);
+}
+
+long mul(long a, long b)
+{
+	return (a * b);
+}
+
+long div(long a, long b)
+{
+	return (a / b);
+}
+
+long mod(long a, long b)
+{
+	return (a % b);
+}
