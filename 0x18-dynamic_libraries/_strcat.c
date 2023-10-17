57749399b0b6f387112908990f228a5b68bfddad
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-10-17 03:36:59 +0100

    String cat.

---
+++ 0x18-dynamic_libraries/_strcat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strcat - I don't know
+ * @dest: destination
+ * @src: source
+ *
+ * Return: Don't know yet
+ *
+ * By: Sina Mathew
+ */
+char *_strcat(char *dest, char *src)
+{
+	return (0);
+}
+
