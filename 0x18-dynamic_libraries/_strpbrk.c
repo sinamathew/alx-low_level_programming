f97e2cc41483b9160b39f7eff6eb71c2ae37f125
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-10-17 04:09:13 +0100

    STRPBRK

---
+++ 0x18-dynamic_libraries/_strpbrk.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strpbrk - function pointer to a string
+ * @s: pointer
+ * @accept: pointer
+ *
+ * Return: I don't know
+ *
+ * By: Sina Mathew
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	return (0);
+}
+
