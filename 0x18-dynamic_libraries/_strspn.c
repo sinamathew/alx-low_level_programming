324fa1c549e12b5e3f987ebdc450b0b7149b624c
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-10-17 04:05:23 +0100

    STRSPN

---
+++ 0x18-dynamic_libraries/_strspn.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strspn - I don't know
+ * @s: I don't know
+ * @accept: smae thing
+ *
+ * Return: Don't know yet
+ *
+ * By: Sina Mathew
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	return (0);
+}
+
