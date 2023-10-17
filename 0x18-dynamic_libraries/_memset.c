69ccc58eb5ee7432f5fe9361ab088ba5a9372104
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-10-17 03:51:07 +0100

    Memory Setup

---
+++ 0x18-dynamic_libraries/_memset.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _memset - setup memory
+ * @s: I don't know
+ * @b: Same thing
+ * @n: Same thing
+ *
+ * Return: I don't know yet
+ *
+ * By: Sina Mathew
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	return (0);
+}
