067df08d326218a7820786a93bc61f192e77284d
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-10-17 03:59:03 +0100

    String to Char

---
+++ 0x18-dynamic_libraries/_strchr.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strchr - string to char
+ * @s: pointer to string
+ * @c: char
+ *
+ * Return: I don't know yet
+ *
+ * By: Sina Mathew
+ */
+char *_strchr(char *s, char c)
+{
+	return (0);
+}
