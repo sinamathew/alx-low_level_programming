ab21c3ef592881d6e0c748c913536282428c36ac
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-10-17 03:47:20 +0100

    String CMP

---
+++ 0x18-dynamic_libraries/_strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strcmp - don't know yet
+ * @s1: pointer to something 1
+ * @s2: pointer to something 2
+ *
+ * Return: I don't know
+ *
+ * By: Sina Mathew
+ */
+int _strcmp(char *s1, char *s2)
+{
+	return (0);
+}
