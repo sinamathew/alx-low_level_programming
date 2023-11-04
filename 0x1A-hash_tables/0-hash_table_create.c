84218218b5c3c7dd57636cfe0b29e821e379b649
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-11-04 02:42:42 +0100

    hot_fix

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,9 +12,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table = NULL;
-
-	if (size == 0)
-		return (NULL);
+	unsigned long int hash;
 
 	new_table = malloc(sizeof(hash_table_t));
 	if (new_table == NULL)
@@ -24,10 +22,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 	new_table->array = malloc(sizeof(hash_node_t *) * size);
 
 	if (new_table->array == NULL)
-	{
-		free(new_table);
 		return (NULL);
-	}
+
+	for (hash = 0; hash < size; hash++)
+		new_table->array[hash] = NULL;
 
 	return (new_table);
 }
