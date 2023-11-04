75a3c6559815623e381ff993ac6009bcc2fdc49d
Sina Mathew <mathewoluwashinastephen@gmail.com>
2023-11-04 01:57:54 +0100

    Hash_Table_Create

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table
+ * @size: size of the array
+ *
+ * Return: a pointer to the newly created hash table.
+ *	NULL if anything goes wrong.
+ *
+ * By: Sina Mathew
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new_table = NULL;
+
+	if (size == 0)
+		return (NULL);
+
+	new_table = malloc(sizeof(hash_table_t));
+	if (new_table == NULL)
+		return (NULL);
+
+	new_table->size = size;
+	new_table->array = malloc(sizeof(hash_node_t *) * size);
+
+	if (new_table->array == NULL)
+	{
+		free(new_table);
+		return (NULL);
+	}
+
+	return (new_table);
+}
